62d96f0e7e5b1915342192ca895ed1e4049f622b
oliver maule <p2793357@my365.dmu.ac.uk>
2024-04-15 22:35:14 +0100

    task2 final finished 	modified:   task2.bash

--- task2.bash
+++ task2.bash
@@ -1,2 +1,21 @@
 #! /usr/bin/bash
 
+USERS=$(cut -d: -f1 /etc/passwd)
+
+REPORT=$(mktemp)
+
+printf "%-20s %-10s %-10s %-10s\n" "USER" "SETUID" "SETGID" "WORLD-WRITEABLE" > $REPORT
+
+for USER in $USERS; do
+    SETUID_FILES=$(find / -user $USER -perm -4000 -type f 2>/dev/null)
+    SETGID_FILES=$(find / -user $USER -perm -2000 -type f 2>/dev/null)
+    WORLD_WRITABLE_FILES=$(find / -user $USER -perm -2 ! -type l -type f 2>/dev/null)
+
+    echo "Setuid files:"
+    echo "$SETUID_FILES"
+    echo "Setgid files:"
+    echo "$SETGID_FILES"
+    echo "World-writeable files:"
+    echo "$WORLD_WRITABLE_FILES"
+    echo "---------------------------------------"
+done
